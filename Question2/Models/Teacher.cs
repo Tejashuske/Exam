29f07e847a3f979d466024b48c296debe7110f53
Tejashuske <127583360+Tejashuske@users.noreply.github.com>
2025-01-14 16:38:14 +0530

    Add files via upload

---
+++ Question2/Models/Teacher.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Question2.Models;
+
+public partial class Teacher
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public string Qualification { get; set; } = null!;
+
+    public DateOnly DateOfJoining { get; set; }
+}
